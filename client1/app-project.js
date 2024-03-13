11e27912ecdcfe3d340f09687f655ea0246d02fa
SeherKumsar <seherkumsar99@outlook.com>
2024-03-14 01:13:54 +0300

    fixed port problems for provider app

--- client1/app-project.js
+++ client1/app-project.js
@@ -10,68 +10,17 @@ amqp.connect('amqp://rabbitmq', function(error0, connection) {
         }
 
         const queue = 'task_queue';
+        const msg = 'Hello World!';
 
         channel.assertQueue(queue, {
             durable: false
         });
 
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-
-        channel.consume(queue, function(msg) {
-            console.log(" [x] Received %s", msg.content.toString());
-        }, {
-            noAck: true
-        });
+        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
     });
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
 });
-
-// app.get("/student/add/:first_name/:last_name/:grade", (req, res) => {
-//   const { first_name, last_name, grade } = req.params;
-//   pool.query("INSERT INTO students (first_name, last_name, grade) VALUES ($1, $2, $3)", [first_name, last_name, grade], (err, results) => {
-//       if (err) {
-//           console.error("Database query error: ", err);
-//           return res.status(500).json({
-//               status: "error",
-//               message: "Internal Server Error"
-//           });
-//       }
-//       res.status(200).json({
-//           status: "success",
-//           data: results.rows
-//       });
-//   });
-// });
-// app.get("/get_students", (req, res) => {
-//   pool.query("SELECT * FROM students", (err, results) => {
-//       if (err) {
-//           console.log("Database query error: ", err);
-//           return res.status(500).json({
-//               status: "error",
-//               message: "Internal Server Error"
-//           });
-//       }
-//       res.status(200).json({
-//           status: "success",
-//           data: results.rows,
-//       });
-//   });
-// });
-// app.get("/students/:id", (req, res) => {
-//   const id = req.params.id;
-//   pool.query("SELECT * FROM students WHERE id = $1", [id], (err, results) => {
-//       if (err) {
-//           console.log("Database query error: ", err);
-//           return res.status(500).json({
-//               status: "error",
-//               message: "Internal Server Error"
-//           });
-//       }
-//       res.status(200).json({
-//           status: "success",
-//           data: results.rows,
-//       });
-//   });
-// });
-// app.listen(2000, () => {
-//   console.log("Server is running on port 2000");
-// });
--- provider-app/provider-app.js
+++ provider-app/provider-app.js
@@ -1,27 +1,63 @@
-// sending messages to rabbitmq -> client1 app
+const express = require('express');
 const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
+const app = express();
+const port = 6001;
+
+app.post('/send', (req, res) => {
+    amqp.connect('amqp://rabbitmq', function(error0, connection) {
+        if (error0) {
+            throw error0;
         }
+        connection.createChannel(function(error1, channel) {
+            if (error1) {
+                throw error1;
+            }
 
-        const queue = 'task_queue';
-        const msg = 'Hello World!';
+            const queue = 'task_queue';
+            const msg = 'Hello World!';
 
-        channel.assertQueue(queue, {
-            durable: false
+            channel.assertQueue(queue, {
+                durable: false
+            });
+
+            channel.sendToQueue(queue, Buffer.from(msg));
+            console.log(" [x] Sent %s", msg);
         });
-
-        channel.sendToQueue(queue, Buffer.from(msg));
-        console.log(" [x] Sent %s", msg);
+        setTimeout(function() {
+            connection.close();
+        }, 500);
     });
-    setTimeout(function() {
-        connection.close();
-        process.exit(0);
-    }, 500);
+    res.send('Message sent!');
+});
+
+app.get('/send', (req, res) => {
+    amqp.connect('amqp://rabbitmq', function(error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function(error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+
+            const queue = 'task_queue';
+            const msg = 'Hello World!';
+
+            channel.assertQueue(queue, {
+                durable: false
+            });
+
+            channel.sendToQueue(queue, Buffer.from(msg));
+            console.log(" [x] Sent %s", msg);
+        });
+        setTimeout(function() {
+            connection.close();
+        }, 500);
+    });
+    res.send('Message sent!');
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
 });
